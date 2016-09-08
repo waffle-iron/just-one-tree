a2afb2118973d563f0947bc8247d48a64f76e0e2
sarah <sarah@triggersandsparks.com>
2016-09-08 08:11:19 -0400

    Add post-count leader-dots styling to widgets.

--- inc/extras.php
+++ inc/extras.php
@@ -45,3 +45,23 @@ function justonetree_add_more_to_nav( $items, $args ) {
 	return $items;
 }
 add_filter( 'wp_nav_menu_items', 'justonetree_add_more_to_nav', 10, 2 );
+
+/**
+ * Filter the categories archive widget to add a span around post count
+ */
+function smittenkitchen_cat_count_span( $links ) {
+	$links = str_replace( '</a> (', '</a><span class="post-count">(', $links );
+	$links = str_replace( ')', ')</span>', $links );
+	return $links;
+}
+add_filter( 'wp_list_categories', 'smittenkitchen_cat_count_span' );
+
+/**
+ * Filter the archives widget to add a span around post count
+ */
+function smittenkitchen_archive_count_span( $links ) {
+	$links = str_replace( '</a>&nbsp;(', '</a><span class="post-count">(', $links );
+	$links = str_replace( ')', ')</span>', $links );
+	return $links;
+}
+add_filter( 'get_archives_link', 'smittenkitchen_archive_count_span' );
