13e22d32cb0123f165580d86f0d28d8742227a19
sarah <sarah@triggersandsparks.com>
2016-10-12 20:10:45 +0100

    Split our nav menu into two halves, and float the logo in between.

--- components/navigation/navigation-header.php
+++ components/navigation/navigation-header.php
@@ -1,3 +1,11 @@
 <nav id="site-navigation" class="main-navigation" role="navigation">
-	<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'header-menu' ) ); ?>
+	<?php
+				wp_nav_menu( array(
+					'theme_location'  => 'menu-1',
+					'menu_id'         => 'header-menu',
+					'fallback_cb'     => 'wp_page_menu',
+					'items_wrap'      => '<ul id="%1$s" class="%2$s"><span class="justonetree-split-nav">%3$s</span></ul>',
+					'walker'          => new JustOneTree_Menu(),
+				) );
+			?>
 </nav>
