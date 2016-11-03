55e4e2121b63b488c16338757bf3608c81e1c7b0
sarah <sarah@triggersandsparks.com>
2016-11-03 19:57:36 +0000

    Notify on sass error in gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -21,13 +21,13 @@ var svgstore = require( 'gulp-svgstore' );
 gulp.task( 'styles', function() {
 	return gulp.src( 'assets/stylesheets/style.scss' )
 		.pipe( sourcemaps.init() )
-		.pipe( sass( { style: 'expanded' } ).on( 'error', sass.logError ) )
+		.pipe( sass( { style: 'expanded' } ) )
+		.on( 'error', notify.onError( function( err ) {
+			return "Stylesheet Error in " + err.message;
+		} ) )
 		.pipe( autoprefixer( { browsers: ['last 2 versions', 'ie >= 9'], cascade: false } ) )
 		//.pipe( csscomb() )
 		.pipe( sourcemaps.write( './', { includeContent: false, sourceRoot: 'source' } ) )
-		.on( 'error', function ( err ) {
-			console.error( 'Error!', err.message );
-		} )
 		.pipe( gulp.dest( './' ) )
 		.pipe( livereload() );
 });
