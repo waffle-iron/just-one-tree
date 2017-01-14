c0c453666e99d1361114f809fb20381d020bbfd5
sarah <sarah@triggersandsparks.com>
2017-01-14 19:47:53 +0000

    Nix featured image from post header, since it's used in page header.

--- components/post/content.php
+++ components/post/content.php
@@ -10,14 +10,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( '' != get_the_post_thumbnail() ) : ?>
-		<div class="post-thumbnail">
-			<a href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail( 'justonetree-featured-image' ); ?>
-			</a>
-		</div>
-	<?php endif; ?>
-
 	<header class="entry-header">
 		<?php
 			if ( 'post' === get_post_type() ) :
