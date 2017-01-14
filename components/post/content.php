b7f817848135c02d11a7e2ff723f21344238303f
sarah <sarah@triggersandsparks.com>
2017-01-14 22:05:09 +0000

    Use excerpts rather than full content on archive pages.

--- components/post/content.php
+++ components/post/content.php
@@ -10,6 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
 	<header class="entry-header">
 		<?php
 			if ( 'post' === get_post_type() ) :
@@ -25,11 +26,17 @@
 	</header>
 	<div class="entry-content">
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'justonetree' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+			if ( is_single() ) :
+
+				the_content( sprintf(
+					/* translators: %s: Name of current post. */
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'justonetree' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );
+
+			else :
+				the_excerpt();
+			endif;
 
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'justonetree' ),
