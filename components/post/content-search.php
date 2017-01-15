79aab528302c6aef5c3e2b7ef91d1ecb70276960
sarah <sarah@triggersandsparks.com>
2017-01-15 00:08:34 +0000

    Ensure search results don't show up twice.
    Note: I don't know why this fixes the issue, but it does,
    so I won't look a gift horse in the mouth.
    
    Fixes #46.

--- components/post/content-search.php
+++ components/post/content-search.php
@@ -11,14 +11,13 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
 		<?php if ( 'post' === get_post_type() ) : ?>
 			<?php get_template_part( 'components/post/content', 'meta' ); ?>
 		<?php endif; ?>
+		
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div>
-	<?php get_template_part( 'components/post/content', 'footer' ); ?>
 </article>
