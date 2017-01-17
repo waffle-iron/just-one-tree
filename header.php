4b0b5e1c363484ac727406c836b6add82ddf4655
sarah <sarah@triggersandsparks.com>
2017-01-17 18:55:30 +0000

    Place all lemon sprigs in header .

--- header.php
+++ header.php
@@ -26,6 +26,10 @@
 
 	<img class="justonetree-lemon-sprig left-sprig" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/lemon-sprig1.png" alt="" />
 
+	<img class="justonetree-lemon-sprig centre-sprig" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/lemon-sprig1.png" alt="" />
+
+	<img class="justonetree-lemon-sprig right-sprig" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/lemon-sprig1.png" alt="" />
+
 	<header id="masthead" class="site-header" role="banner">
 
 		<?php get_template_part( 'components/features/hero-image/content', 'hero' ); ?>
